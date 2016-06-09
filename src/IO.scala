62d92782e29fe9203c035d518a9e7941784ba4f1
Jooyung Han <jooyung.han@gmail.com>
2016-06-09 17:27:35 +0900

    Add IO Monad examples with some helpers(**, implicit conversions)

--- src/IO.scala
+++ src/IO.scala
@@ -1,13 +1,14 @@
+import scala.io.StdIn
 import functional._
 import io._
-import scala.io.StdIn
+import conv._
+import player._
+import examples._
 /**
   * Created by jooyung.han on 6/9/16.
   */
 
 object player {
-  import io._
-
   case class Player(name: String, score: Int)
 
   def winnerMsg(p: Option[Player]): String = p map {
@@ -48,10 +49,26 @@ object functional {
     def map2[A,B,C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
       flatMap(ma)(a => map(mb)(b => f(a, b)))
   }
+
+  implicit def operators[F[_], A](fa: F[A]): MonadOps[F, A] = MonadOps[F, A](fa)
+
+  case class MonadOps[F[_], A](fa: F[A]) {
+    def flatMap[B](f: A => F[B])(implicit m: Monad[F]): F[B] = m.flatMap(fa)(f)
+    def map[B](f: A => B)(implicit m: Monad[F]): F[B] = m.map(fa)(f)
+    def **[B](fb: F[B])(implicit m: Monad[F]): F[(A, B)] = m.map2(fa, fb)((_, _))
+  }
+
+  def replicateM[F[_],A](n: Int)(fa: F[A])(implicit m: Monad[F]): F[List[A]] =
+    if (n == 0) m.unit(Nil)
+    else m.map2(fa, replicateM(n-1)(fa))(_ :: _)
+
+  def sequence[F[_], A](actions: List[F[A]])(implicit m: Monad[F]): F[List[A]] = actions match {
+    case Nil => m.unit(Nil)
+    case a::as => m.map2(a, sequence(as))(_ :: _)
+  }
 }
 
 object io {
-
   trait IO[A] { self =>
     def run: A
     def map[B](f: A => B): IO[B] =
@@ -60,7 +77,7 @@ object io {
       new IO[B] { def run = f(self.run).run }
   }
 
-  object IO extends Monad[IO] {
+  implicit object IO extends Monad[IO] {
     def unit[A](a: => A): IO[A] = new IO[A] { def run = a }
     def flatMap[A,B](fa: IO[A])(f: A => IO[B]) = fa flatMap f
     def apply[A](a: => A): IO[A] = unit(a)
@@ -68,23 +85,23 @@ object io {
 
   def ReadLine: IO[String] = IO { StdIn.readLine }
   def PrintLine(msg: String): IO[Unit] = IO { println(msg) }
+}
 
-  def sequence[A](actions: List[IO[A]]): IO[List[A]] = actions match {
-    case Nil => IO.unit(Nil)
-    case a::as => IO.map2(a, sequence(as))(_ :: _)
-  }
+object examples {
+  val echo = ReadLine.flatMap(PrintLine)
+  val readInt = ReadLine.map(_.toInt)
+  val readInts = readInt ** readInt
+  val read5Lines: IO[List[String]] = replicateM(5)(ReadLine)
 }
 
 object Main extends App {
-  import player._
-  import io._
-
   val x: IO[Unit] = contest(Player("LG", 3), Player("GS", 2))
   x.run
 
   val helloWorld: IO[List[Unit]] = sequence(List(PrintLine("Hello"), PrintLine("World")))
   helloWorld.run
 
-  import conv._
   converter.run
+
+  read5Lines.flatMap(lines => PrintLine(lines.mkString("\n"))).run
 }
